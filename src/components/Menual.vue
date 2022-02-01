5c100458a41d8252e6e420f4429de6d280fa41d5
leejungho9 <9wjdgh9@naver.com>
2022-02-01 23:52:59 +0900

    Add step3

--- src/components/Menual.vue
+++ src/components/Menual.vue
@@ -96,106 +96,57 @@
                <h4 class= "step2_title"> {{steps2[i].title}}</h4>
                <h4 class="step2_content"> {{steps2[i].content}}</h4>
               </div>
-
-
             </div>
           </div>
-
-
         </div>
-
-      
       </div>
     </div>
 
     <!--33333333333333333333333333333333333333333333333333333-->
     <!--CONTENT3-->
-    <div id="tab3" class="tab-conent items" v-if="step == 2">
+    <div id="tab3" class="tab-conent items" data-tab-content v-if="step == 2">
       <div class="step1">
         <p class="step1__title">STEP3</p>
         <p class="step1_title__sub1">내 옷의 진행 상황을 마이페이지에서 확인할 수 있어요.</p>
         <p class="step1_title__sub2">
-          마이페이지이 이용내역에서 확인해주세요.
-        </p>
+         마이페이지의 이용내역에서 확인해주세요.</p>
       </div>
       <!--SERVICE-->
-      <div class="step1_service">
+      <div class="step3_service">
         <!-- SERVICE1 -->
         <div class="service">
           <div class="service__content">
-            <p class="service__title service_title_1">개별 클리닉 서비스</p>
-            <p class="service__title_sub service_title_sub_1">
-              배달완료드라이클리닝뿐만 아니라 웨트클리닝까지. 개별 품목의 권장된
-              케어라벨에 따라 세탁하고, 오염 제거를 위한 다양한 케어가
-              포함됩니다.
-            </p>
-          </div>
-
-          <div class="service_img service1_img">
-            <img src="../assets/service1.png" alt="서비스1" />
-          </div>
-        </div>
-
-        <!-- SERVICE2 -->
-        <div class="service">
-          <div class="service__content">
-            <p class="service__title service_title_2">생활 빨래 서비스</p>
-            <p class="service__title_sub service_title_sub_2">
-              봉투에 수건, 양말, 잠옷 같은 빨랫감을 담아주시면 단독 물세탁하고
-              보송하게 개어 드립니다
-            </p>
-          </div>
-
-          <div class="service_img service2_img">
-            <img src="../assets/service2.png" alt="서비스2" />
+              <img src="../assets/step3.png" alt="">
           </div>
         </div>
       </div>
     </div>
-    <!--444444444444444444444444444444444444444-->
+
+
+    <!--444444444444444444444444-->
     <!--CONTENT4-->
-    <div id="tab4" class="tab-conent items" v-if="step == 3">
+    <div id="tab4" class="tab-conent items" data-tab-content v-if="step == 3">
       <div class="step1">
         <p class="step1__title">STEP4</p>
         <p class="step1_title__sub1">세탁된 옷이 문앞에 도착해요.</p>
         <p class="step1_title__sub2">
-          신청한 배송 방식에 따라 런드렛 혹은 라이트 백에 세탁물이 담겨 배송됩니다. <br>
-          수선을 맡기는 경우 배송기간이 조금 더 소요되며, 세탁과 수선을 함께 맡기는 경우는 수선 완료 시 함께 배송됩니다.
+         신청한 배송 방식에 따라 런드렛 혹은 라이트 백에 세탁물이 담겨 배송됩니다. <br>
+         수선을 맡기는 경우 배송기간이 조금 더 소요되며, 세탁과 수선을 함께 맡기는 경우는 수선 완료 시 함께 배송됩니다.
         </p>
       </div>
       <!--SERVICE-->
-      <div class="step1_service">
+      <div class="step4_service">
         <!-- SERVICE1 -->
         <div class="service">
           <div class="service__content">
-            <p class="service__title service_title_1">세탁된 옷이 문앞에 도착해요.</p>
-            <p class="service__title_sub service_title_sub_1">
-              신청한 배송 방식에 따라 런드렛 혹은 라이트 백에 세탁물이 담겨 배송됩니다.
-              수선을 맡기는 경우 배송기간이 조금 더 소요되며, 세탁과 수선을 함께 맡기는 경우는 수선 완료 시 함께 배송됩니다.
-            </p>
-          </div>
-
-          <div class="service_img service1_img">
-            <img src="../assets/service1.png" alt="서비스1" />
-          </div>
-        </div>
-
-        <!-- SERVICE2 -->
-        <div class="service">
-          <div class="service__content">
-            <p class="service__title service_title_2">생활 빨래 서비스</p>
-            <p class="service__title_sub service_title_sub_2">
-              봉투에 수건, 양말, 잠옷 같은 빨랫감을 담아주시면 단독 물세탁하고
-              보송하게 개어 드립니다
-            </p>
-          </div>
-
-          <div class="service_img service2_img">
-            <img src="../assets/service2.png" alt="서비스2" />
+              <img src="../assets/step4.png" alt="">
           </div>
         </div>
       </div>
     </div>
+
+
+
   </div>
 </template>
 
@@ -395,7 +346,7 @@ export default {
 .manual .tab-conent .step2_service {
   width : 700px;
   min-height: 450px;
-  background-color: #ebe9e1;
+  background-color: #fbf9ee;
   border-radius: 20px;
   position: relative;
   margin: auto;
@@ -428,6 +379,16 @@ export default {
 }
 .step2Area .step2_title {
   font-weight: 600;
+}
+/*step3_ service */
+.step3_service .service__content img{
+  border-radius: 20px;
+  width: 700px;
+}
+/*step4_ service */
+
+.step4_service .service__content img{
+  border-radius: 20px;
 }
 /*scroll text event*/
 .fromTopIn
